fae14eec6dbbc0ba335295e2c7ee3cb5f7a81d86
Toby Bradshaw <tobybrad@microsoft.com>
2017-04-24 12:22:37 +0100

    Add boilerplate for calling LUIS, dealing with cnf

--- app.js
+++ app.js
@@ -1,30 +1,65 @@
+var nconf = require('nconf');
+var request = require('request');
 var restify = require('restify');
 var builder = require('botbuilder');
+
+var config = nconf.env().argv().file({file: 'localConfig.json'});
+
+//=========================================================
+// Utility functions
+//=========================================================
+
+function _askLUIS(appId, subKey, q) {
+    var uri = `https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/${appId}?subscription-key=${subKey}&verbose=true&q=${q}`;
+
+    return new Promise((resolve, reject) => {
+        var options = {
+            uri: uri,
+            method: 'GET'
+        };
+        request(options, (err, response, body) => {
+            resolve(JSON.parse(body));
+        })
+    })
+}
+
+function askLUIS(q) {
+    return _askLUIS(config.get("LUIS_APP_ID"), config.get("LUIS_SUBSCRIPTION_KEY"), q);
+}
 
 //=========================================================
 // Bot Setup
 //=========================================================
 
-// Setup Restify Server
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, '0.0.0.0', function () {
-   console.log('%s listening to %s', server.name, server.url); 
-});
-  
-// Create chat bot
-var connector = new builder.ChatConnector({
-  appId: process.env.MICROSOFT_APP_ID,
-  appPassword: process.env.MICROSOFT_APP_PASSWORD
-});
-//var connector = new builder.ConsoleConnector().listen()
-var bot = new builder.UniversalBot(connector);
-server.post('/api/messages', connector.listen());
+function main() {
 
-//=========================================================
-// Bots Dialogs
-//=========================================================
+    // Setup Restify Server
+    var server = restify.createServer();
+    server.listen(process.env.port || process.env.PORT || 3978, '0.0.0.0', function () {
+      console.log('%s listening to %s', server.name, server.url); 
+    });
+      
+    // Create chat bot
+    var connector = new builder.ChatConnector({
+      appId: process.env.MICROSOFT_APP_ID,
+      appPassword: process.env.MICROSOFT_APP_PASSWORD
+    });
 
-bot.dialog('/', function (session) {
-    session.send("Hello World");
+    //var connector = new builder.ConsoleConnector().listen()
+    var bot = new builder.UniversalBot(connector);
+    server.post('/api/messages', connector.listen());
+
+    //=========================================================
+    // Bots Dialogs
+    //=========================================================
+
+    bot.dialog('/', function (session) {
+        session.send("Hello World");
+    });
+}
+
+main();
+askLUIS("updates on microsoft")
+.then((result) => {
+    console.log(result);
 });
-
